18e1d013e6172292a6a26e7d85d8adc608ef40df
Daksh Patel <34683942+daksh2298@users.noreply.github.com>
2020-05-04 22:35:01 -0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 __author__ = "Daksh Patel"
 
-aws_access_key_id = 'AKIA5WTK4BJPARQ7JDBC'
-aws_secret_access_key  = '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'
+aws_access_key_id = 'ACCESS KEY'
+aws_secret_access_key  = 'SECRET-ACCESS-KEY'
 region = 'us-east-2'
